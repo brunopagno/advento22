ef492afe405086a32da5874ee2ffa0d6e6e12120
Bruno Pagno <brunopagno@gmail.com>
2022-12-03 14:16:52 +0100

    removed ruby stuff because obviouly, who has that much patience?

--- util/data.go
+++ util/data.go
@@ -6,7 +6,7 @@ import (
 )
 
 func InputDataForDay(day int) string {
-	in, err := os.ReadFile(fmt.Sprintf("../data/day%d.txt", day))
+	in, err := os.ReadFile(fmt.Sprintf("./data/day%d.txt", day))
 	if err != nil {
 		panic(err)
 	}
